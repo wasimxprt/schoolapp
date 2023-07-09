e68bcc52ee11ff283f2ae4a0fa60055d78b405e9
Wasim Sayyed <wasimxprt@gmail.com>
2023-07-09 15:37:57 +0530

    Adding Swagger ui for documentation

--- src/main/java/com/schoolapp/SchoolappApplication.java
+++ src/main/java/com/schoolapp/SchoolappApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "School app Spring Boot application", description = "School app Spring Boot application", version = "v1.0", contact = @Contact(name = "Wasim", email = "wasimxprt@gmail.com")))
 public class SchoolappApplication {
 
 	@Bean
--- src/main/java/com/schoolapp/controller/UserController.java
+++ src/main/java/com/schoolapp/controller/UserController.java
@@ -24,8 +24,12 @@ import com.schoolapp.exception.ErrorDetails;
 import com.schoolapp.exception.ResourceNotFoundException;
 import com.schoolapp.service.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
+@Tag(name = "CRDU rest apis for User resources", description = "CRUD apis for get, post, put, delete")
 @RestController
 @RequestMapping("api/user")
 public class UserController {
@@ -34,30 +38,41 @@ public class UserController {
 	private UserService userService;
 
 	// create user
+	@Operation(summary = "Create user REST api", description = "Create User API is used to save user in the database")
+	@ApiResponse(responseCode = "201", description = "HTTP Status 201 CREATED")
 	@PostMapping("")
 	public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto user) {
 		UserDto savedUser = userService.createUser(user);
 		return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
 	}
 
+	@Operation(summary = "Create user by id REST api", description = "Create User API is used to get a single user from the database")
+	@ApiResponse(responseCode = "200", description = "HTTP Status 200 SUCCESS")
 	@GetMapping("/{id}")
 	public ResponseEntity<UserDto> getUserById(@PathVariable("id") Long id) {
 		UserDto user = userService.getUserById(id);
 		return new ResponseEntity<>(user, HttpStatus.OK);
 	}
 
+	@Operation(summary = "Create all users REST api", description = "Create User API is used to get all users from the database")
+	@ApiResponse(responseCode = "200", description = "HTTP Status 200 SUCCESS")
 	@GetMapping("")
 	public ResponseEntity<List<UserDto>> getAllUsers() {
 		List<UserDto> users = userService.getAllUsers();
 		return new ResponseEntity<>(users, HttpStatus.OK);
 	}
 
+	@Operation(summary = "Update user REST api", description = "Create User API is used to get all users from the database")
+	@ApiResponse(responseCode = "200", description = "HTTP Status 200 SUCCESS")
 	@PutMapping("/{id}")
 	public ResponseEntity<UserDto> updateUser(@PathVariable("id") Long id, @Valid @RequestBody UserDto user) {
 		user.setId(id);
 		UserDto updatedUser = userService.updateUser(user);
 		return new ResponseEntity<>(updatedUser, HttpStatus.OK);
 	}
+	
+	@Operation(summary = "Delete user REST api", description = "Delete User API is used to delete user from the database")
+	@ApiResponse(responseCode = "200", description = "HTTP Status 200 SUCCESS")
 
 	@DeleteMapping("/{id}")
 	public ResponseEntity<String> deleteUser(@PathVariable("id") Long userId) {
@@ -65,5 +80,4 @@ public class UserController {
 		return new ResponseEntity<>("User deleted successfully!", HttpStatus.OK);
 	}
 
-	
 }
