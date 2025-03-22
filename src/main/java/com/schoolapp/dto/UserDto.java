2afc2a50500c36c8edc985f2b11b8ddabc93d1b2
wasimxprt <wasimxprt@gmail.com>
2025-03-22 23:38:18 +0300

    Update UserDto.java

--- src/main/java/com/schoolapp/dto/UserDto.java
+++ src/main/java/com/schoolapp/dto/UserDto.java
@@ -28,51 +28,6 @@ public class UserDto {
 	@Schema(description="User email address")
 	@NotEmpty(message = "Please enter email")
 	@Email(message = "Please a enter valid email")
-	private String email;
-
-	public UserDto() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public UserDto(Long id, String firstName, String lastName, String email) {
-		super();
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
+	private String email;	
 
 }
